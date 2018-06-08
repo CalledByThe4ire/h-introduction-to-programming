ea9d053c2d824f2ae2326a741103430cf12d1bd6
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-08 23:42:51 +0300

    add solution

--- src/sequenceSum.js
+++ src/sequenceSum.js
@@ -1,6 +1,10 @@
 // @flow
 
-const sequenceSum = (begin: number, end: number): number => {};
+const sequenceSum = (begin: number, end: number): number => {
+  if (begin > end) return NaN;
+  if (begin === end) return begin;
+  return end + sequenceSum(begin, end - 1);
+};
 
 export default sequenceSum;
 
