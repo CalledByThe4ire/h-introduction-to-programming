bb5f5da36c8146a8ad41b8bb3a87cb5670f4af32
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-09 23:42:41 +0300

    add solution

--- src/smallestDivisor.js
+++ src/smallestDivisor.js
@@ -1,6 +1,17 @@
 // @flow
 
-const smallestDivisor = (n: number): number => {};
+const smallestDivisor = (n: number): number => {
+  const iter = (acc: number): number => {
+    if (n === 1) {
+      return 1;
+    }
+    if (n % acc === 0) {
+      return acc;
+    }
+    return iter(acc + 1);
+  };
+  return iter(2);
+};
 
 export default smallestDivisor;
 
