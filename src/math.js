ae40840a2e7b2e28a955d0c5b4292f5d07a7e6b7
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-21 19:02:07 +0300

    add hexlet' solution

--- src/math.js
+++ src/math.js
@@ -1,8 +1,5 @@
-/* eslint-disable no-unused-vars, import/no-duplicates */
 import cube from './numbers1';
-import multi from './numbers2';
-import { pow2 as pow, sum2 as sum, sub2 as sub } from './numbers2';
+import multi, { pow2 as pow, sum2 as sum, sub2 as sub } from './numbers2';
 import sqrt from './numbers3';
 
-export { pow, sum, sub, multi, sqrt };
-export default cube;
+export { cube as default, multi, pow, sum, sub, sqrt };
