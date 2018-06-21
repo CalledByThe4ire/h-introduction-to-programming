3289a40acbed92a8287e44b0be04313228ab7106
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-21 19:00:44 +0300

    add solution

---
+++ src/math.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars, import/no-duplicates */
+import cube from './numbers1';
+import multi from './numbers2';
+import { pow2 as pow, sum2 as sum, sub2 as sub } from './numbers2';
+import sqrt from './numbers3';
+
+export { pow, sum, sub, multi, sqrt };
+export default cube;
