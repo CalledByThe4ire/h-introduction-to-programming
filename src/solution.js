c597fdfc37dea10bc0bda0955d4ff0cbc088f3b8
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-22 18:05:41 +0300

    add solution

--- src/solution.js
+++ src/solution.js
@@ -1,3 +1,4 @@
+/* eslint-disable function-paren-newline */
 // @flow
 const permutator = (inputArr: Array<mixed>): Array<mixed[]> => {
   const result: Array<mixed[]> = [];
@@ -19,3 +20,46 @@ const permutator = (inputArr: Array<mixed>): Array<mixed[]> => {
   return result;
 };
 
+const unique = (arr: Array<mixed>): Array<mixed> =>
+  arr.filter(
+    (element: mixed, index: number, array: Array<mixed>) =>
+      array.indexOf(element) === index,
+  );
+
+const make = (zero: number, one: number): Array<mixed> => {
+  let x: number = one;
+  let y: number = zero;
+
+  const unit: number = 1;
+  const nil: number = 0;
+
+  const arr: Array<any> = [];
+
+  while (x !== 0) {
+    arr.push(unit);
+    x -= 1;
+  }
+
+  while (y !== 0) {
+    arr.push(nil);
+    y -= 1;
+  }
+
+  return arr;
+};
+
+const stringify = (arr: Array<mixed[]>): Array<mixed> =>
+  arr.map(element => element.toString());
+const removeCopies = (arr: Array<mixed>): Array<mixed> => unique(arr);
+const filterZero = (arr: Array<mixed>): Array<mixed> =>
+  arr.filter(element => element.match(/(0,?\s?){2}/g));
+const findDiff = (arr1: Array<mixed>, arr2: Array<mixed>): number =>
+  arr1.length - arr2.length;
+
+const withoutTwoZeros = (a: number, b: number): number => {
+  const permutationsList: Array<mixed> = removeCopies(stringify(permutator(make(a, b))));
+  const filtered: Array<mixed> = filterZero(permutationsList);
+  return findDiff(permutationsList, filtered);
+};
+
+export default withoutTwoZeros;
