ca1748977daf5bd390c5d6237de39c62b7a19359
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-25 19:08:08 +0300

    add solution

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,18 @@
 // BEGIN (write your solution here) (write your solution here)
 // @flow
+const fizzBuzz = (begin: number, end: number) => {
+  for (let i: number = begin; i < end; i += 1) {
+    if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    }
+    console.log(i);
+  }
+};
+
 // END
 
 export default fizzBuzz;
