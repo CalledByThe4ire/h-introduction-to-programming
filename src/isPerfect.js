880e56747acf2026b2ecc74a2f67c66d1bebf19a
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-25 17:37:11 +0300

    add solution

--- src/isPerfect.js
+++ src/isPerfect.js
@@ -1,7 +1,17 @@
 // BEGIN (write your solution here) (write your solution here)
 // @flow
-const isPerfect = (number: number): boolean => {
-  
+const isPerfect = (n: number): boolean => {
+  if (n <= 0) {
+    return false;
+  }
+
+  let sum = 0;
+  for (let i: number = 1; i <= n / 2; i += 1) {
+    if (n % i === 0) {
+      sum += i;
+    }
+  }
+  return sum === n;
 };
 // END
 
