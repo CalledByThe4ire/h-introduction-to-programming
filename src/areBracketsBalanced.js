922dffa46f355ac95dda383bf449abbbac13cfa4
Andrey Karamnoff <karamnoff@gmail.com>
2018-07-02 14:52:21 +0300

    add hexlet' solution

--- src/areBracketsBalanced.js
+++ src/areBracketsBalanced.js
@@ -1,35 +1,17 @@
-/* eslint-disable no-constant-condition */
+
 import { length } from './strings';
 
 // BEGIN (write your solution here)
 // @flow
-const isEven = (n: number): boolean => {
-  const num: number = Number(n);
-  return num === 0 || !!(num && !(num % 2));
-};
-
-const countTxtEntries = (str: string, substr: string): number => {
-  let pos: number = 0;
-  let counter: number = 0;
-
-  while (true) {
-    const foundPos: number = str.indexOf(substr, pos);
-
-    if (foundPos === -1) {
-      break;
-    }
-
-    counter += 1;
-    pos = foundPos + 1;
-  }
-
-  return counter;
-};
-
 export default (str: string): boolean => {
-  if (!isEven(length(str))) {
-    return false;
+  let acc: number = 0;
+  for (let i: number = 0; i < length(str); i += 1) {
+    const symbol: string = str[i];
+    acc = symbol === '(' ? acc + 1 : acc - 1;
+    if (acc < 0) {
+      return false;
+    }
   }
-  return countTxtEntries(str, '(') === countTxtEntries(str, ')');
+  return acc === 0;
 };
 // END
