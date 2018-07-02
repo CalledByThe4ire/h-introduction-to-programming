a12dd14cab420ef25465e898e6e95308b57dbf89
Andrey Karamnoff <karamnoff@gmail.com>
2018-07-02 14:48:35 +0300

    add solution

--- src/areBracketsBalanced.js
+++ src/areBracketsBalanced.js
@@ -1,5 +1,35 @@
+/* eslint-disable no-constant-condition */
 import { length } from './strings';
 
 // BEGIN (write your solution here)
+// @flow
+const isEven = (n: number): boolean => {
+  const num: number = Number(n);
+  return num === 0 || !!(num && !(num % 2));
+};
 
+const countTxtEntries = (str: string, substr: string): number => {
+  let pos: number = 0;
+  let counter: number = 0;
+
+  while (true) {
+    const foundPos: number = str.indexOf(substr, pos);
+
+    if (foundPos === -1) {
+      break;
+    }
+
+    counter += 1;
+    pos = foundPos + 1;
+  }
+
+  return counter;
+};
+
+export default (str: string): boolean => {
+  if (!isEven(length(str))) {
+    return false;
+  }
+  return countTxtEntries(str, '(') === countTxtEntries(str, ')');
+};
 // END
